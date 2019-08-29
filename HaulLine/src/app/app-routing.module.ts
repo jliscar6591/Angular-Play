dbd08be84a8092295a0bc9033d852984e76c576c
John Liscar <johnliscar@Johns-MacBook-Pro.local>
2019-08-28 21:59:14 -0400

    working login and jobs list page

--- HaulLine/src/app/app-routing.module.ts
+++ HaulLine/src/app/app-routing.module.ts
@@ -1,33 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { JobsComponent } from './jobs/jobs.component';
-import { HistoryComponent } from './history/history.component';
-import { AccountComponent } from './account/account.component';
-import { CreateComponent } from './create/create.component';
+import { JobsComponent } from './site/jobs/jobs.component';
+import { HistoryComponent } from './site/history/history.component';
+import { AccountComponent } from './site/account/account.component';
+import { CreateComponent } from './site/create/create.component';
+import { SiteComponent }from './site/site.component';
 
 
 const routes: Routes = [
   {
-    path: 'jobs',
-    component: JobsComponent
-  },
-  {
-    path: 'history',
-    component: HistoryComponent
-  },
-  {
-    path: 'account',
-    component: AccountComponent
-  },
-  {
-    path: 'create',
-    component: CreateComponent
-  },
-  {
     path: '',
+    component: SiteComponent,
+    children:[
+      {
+        path: 'jobs',
+        component: JobsComponent
+      },
+      {
+        path: 'history',
+        component: HistoryComponent
+      },
+      {
+        path: 'account',
+        component: AccountComponent
+      },
+      {
+        path: 'create',
+        component: CreateComponent
+      }
+    ]
+  },
+  {
+    path: 'welcome',
     component: LoginComponent
   },
+  {
+    path: '**',
+    component: LoginComponent
+  }
 ];
 
 @NgModule({
--- HaulLine/src/app/app.module.ts
+++ HaulLine/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AccountComponent } from './account/account.component';
-import { HistoryComponent } from './history/history.component';
-import { CreateComponent } from './create/create.component';
-import { JobsComponent } from './jobs/jobs.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatCardModule} from '@angular/material/card';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatChipsModule} from '@angular/material/chips';
+import { AccountComponent } from './site/account/account.component';
+import { HistoryComponent } from './site/history/history.component';
+import { CreateComponent } from './site/create/create.component';
+import { JobsComponent } from './site/jobs/jobs.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { SiteComponent } from './site/site.component';
+import {MatListModule} from '@angular/material/list';
+import {MatBadgeModule} from '@angular/material/badge';
+
+
 
 
 
@@ -29,26 +35,25 @@ import {MatChipsModule} from '@angular/material/chips';
     CreateComponent,
     HistoryComponent,
     JobsComponent,
-    AccountComponent
+    AccountComponent,
+    SiteComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatInputModule,
     MatChipsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatRadioModule,
+    MatListModule,
+    MatIconModule,
     MatCardModule,
+    MatBadgeModule,
     MatMenuModule,
     MatExpansionModule,
     MatButtonModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      { path: '', component: LoginComponent},
-      { path: 'account', component: AccountComponent},
-      { path: 'create', component: CreateComponent},
-      { path: 'history', component: HistoryComponent},
-      { path: 'jobs', component: JobsComponent}
-    ]),
     BrowserAnimationsModule
   ],
   providers: [],
